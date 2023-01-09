d29dab5f1a344caae003a0c519c635a86d9c1668
Arun-Tiwary <70338393+Arun-Tiwary@users.noreply.github.com>
2023-01-09 20:23:12 +0530

    feat: header component

---
+++ src/Components/footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        <p>This is a copyright message</p>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ src/Components/note.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Notes = () => {
+  return (
+    <div>
+        <h1>Title</h1>
+        <p>Content</p>
+        <button>Delete</button>
+        
+    </div>
+  )
+}
+
+export default Notes
