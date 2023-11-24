24691f1155b63f70f5304a146067a5387f338cd1
Arun <arun.tiwari@techugo.com>
2023-11-24 11:06:36 +0530

    remove confirm alert css

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Notes from "./components/Note";
 import { toast, Toaster, ToastBar } from "react-hot-toast";
-import "react-confirm-alert/src/react-confirm-alert.css";
+// import "react-confirm-alert/src/react-confirm-alert.css";
 function App() {
   return (
     <div className="App">
